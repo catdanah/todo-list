7d11cd177aad4c0013ad7c1c64b5674e51ba2422
catdanah <xnxjr1219@naver.com>
2021-08-02 17:57:56 +0900

    Add js file

--- js/todo-list.js
+++ js/todo-list.js
@@ -1,1 +1,22 @@
-console.log("hello");
+const todoForm = document.querySelector("#todo-form");
+const todoInput = document.querySelector("#todo-form input");
+const todoList = document.querySelector("#todo-list");
+
+const todos = [];
+
+function paintTodo() {
+    todoInput.value="";
+    const todo = localStorage.getItem("to-do");
+    const li = document.createElement("li");
+    todoList.appendChild(li);
+    li.innerText = `${todo}`;
+
+}
+
+function submitTodo(event) {
+    event.preventDefault();
+    localStorage.setItem("to-do", todoInput.value);
+    paintTodo();
+}
+
+todoForm.addEventListener("submit", submitTodo);
