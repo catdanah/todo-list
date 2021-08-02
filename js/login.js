6048b6e2f2aa8d982782670eb3981b8480d231a4
catdanah <xnxjr1219@naver.com>
2021-08-02 18:00:57 +0900

    Add folder

--- js/login.js
+++ js/login.js
@@ -3,37 +3,40 @@ const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 
 const savedUsername = localStorage.getItem("username");
+// logout button
 const logoutBtn = document.createElement("button");
-const btnTxt = document.createTextNode("logout");
+const logoutBtnTxt = document.createTextNode("logout");
 
-function submitUsername(event) {
-    event.preventDefault();
-    loginForm.classList.add("hidden");
+function login() {
     localStorage.setItem("username", loginInput.value);
     greetUser(loginInput.value);
 }
 
 function greetUser(username) {
+    loginForm.classList.add("hidden");
     greeting.innerText = `환영합니다! ${username}님!`;
-    logoutBtn.appendChild(btnTxt);
+    logoutBtn.appendChild(logoutBtnTxt);
     greeting.appendChild(logoutBtn);
 }
 
+// username 편집 기능 추가
 function logout() {
     localStorage.removeItem("username");
     greeting.classList.add("hidden");
-    login();
+    location.reload();
+    loginProcess();
 }
 
-function login() {
+function loginProcess() {
     if(savedUsername === null) {
         loginForm.classList.remove("hidden");
-        loginForm.addEventListener("submit", submitUsername);
+        // form을 submit하고 input의 value값을 가져옴
+        loginForm.addEventListener("submit", login);
     }else {
         loginForm.classList.add("hidden");
         greetUser(savedUsername);
     }
 }
 
+loginProcess();
 logoutBtn.addEventListener("click", logout);
-login();
